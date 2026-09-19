28ff91632b99a376b16afb453063f9a56af2b958
Rohaeb <rohaebkali@gmail.com>
2026-09-18 21:42:09 -0400

    Update reset.py

--- reset.py
+++ reset.py
@@ -20,9 +20,7 @@ def run_scr(digits):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--single-process")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
     chrome_options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(options=chrome_options)
     print("hello")
